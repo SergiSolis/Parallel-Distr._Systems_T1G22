969b9cd574e7abf818a4a5043aa6e0e3f7e433da
Hong-ming <37633722+homexiang@users.noreply.github.com>
2021-05-09 19:28:48 +0200

    ex1 speedup / outputs

--- LAB 1/lab1_TxGyy/speedup.py
+++ LAB 1/lab1_TxGyy/speedup.py
@@ -5,8 +5,8 @@ import matplotlib.ticker as ticker
 cores = [1, 2, 4, 8, 16]
 axis = [str(cores[i]) for i in range(len(cores))]
 
-times_case1 = [1.0,1.0,1.0,1.0,1.0]
-times_case2 = [1.0,0.9,0.8,0.7,0.6]
+times_case1 = [0.608571,0.305467,0.152663,0.077339,0.038933]
+times_case2 = [0.849875,0.414260,0.207567,0.096535,0.048264]
 
 ideal_speedup = cores
 experimental_speedup_case1 = [ times_case1[0]/x for x in times_case1 ]
@@ -21,8 +21,8 @@ ax.set_xscale('log', basex=2)
 ax.set_yscale('log', basey=2)
 
 plt.plot(cores, ideal_speedup, label='ideal')
-plt.plot(cores, experimental_speedup_case1, label="experimental case 1")
-plt.plot(cores, experimental_speedup_case2, label="experimental case 2")
+plt.plot(cores, experimental_speedup_case1, label="case for")
+plt.plot(cores, experimental_speedup_case2, label="case no for")
 ax.legend(loc='upper left')
 
 ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y),0)))).format(y)))
