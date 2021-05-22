8df063b919b140d00b26170e43ad61784385c0f5
Hong-ming <37633722+homexiang@users.noreply.github.com>
2021-05-22 17:25:28 +0200

    ex1 casi hecho

--- LAB 2/ex1/matrix.c
+++ LAB 2/ex1/matrix.c
@@ -17,23 +17,28 @@ int *new_matrix (int size, int rank)
 
 void init_matrix (int *matrix, int size, int rank)
 {
+	int x = 1;
+	int diagonal;
    //initialize a matrix with size*size dimensions 
-   for (int i=0; i < size; i++){
-      for(int j=0; j < size; j++){
-         matrix[i][j] = 0;
-      }
+   for (int i=0; i < size*size; i++){
+		diagonal = size*x - x;
+		if(i == diagonal && x <= size){
+			matrix[i] = rank * x;
+			x ++;
+		}else{
+			matrix[i] = 0;
+		}
    }
 }
 
 void print_matrix (int *matrix, int size)
 {
    //print the matrix 
-    for (int i=0; i < size; i++){
-       printf(" (");
-      for(int j=0; j < size; j++){
-         printf("%d ", matrix[i][j]);
-      }
-      printf(")\n");
+    for (int i=0; i < size*size; i++){
+	if(i % size == 0){
+		printf("\n");
+	}
+      printf("%d\t", matrix[i]);
    }
 }
 
@@ -44,28 +49,41 @@ int main (int argc, char* argv[])
    int root;
 
    /* MPI Initialization */
-
+    MPI_Init(&argc,&argv );
+	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+	MPI_Comm_size (MPI_COMM_WORLD, &size);
+	root = size - 1;
    /* Matrix creation and initialization */
-
+   
+	matrix = new_matrix(size,rank);
+	result = new_matrix(size,rank);
+	init_matrix(matrix,size,rank);
+	
    /* Print initial root matrix */
    if (rank == root) 
    {
       printf ("Initial Matrix (rank %d)\n", root);
       print_matrix (matrix, size);
    }
-
+	printf("\n");
    /* Datatype vector creation */
-
+   
+	MPI_Datatype antidiagonaltype;
+    MPI_Type_vector(size, 1, size-1, MPI_INT, &antidiagonaltype);
+    MPI_Type_commit(&antidiagonaltype);
+	
    /* Communication: root gathers all the diagonals from the other ranks */
-
+   
+	MPI_Gather(matrix,1,antidiagonaltype,result,size,MPI_INT,root,MPI_COMM_WORLD);
+	
    /* Print final root matrix */
    if (rank == root) 
    {
       printf ("Final matrix (rank %d)\n", root);
       print_matrix (result, size);
    }
-
    /* Remember to free the datatype! */
-
+   MPI_Type_free (&antidiagonaltype);
+   MPI_Finalize();
    return 0;
 }
