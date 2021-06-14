46ffb5fa941cfafb15122609c1145669cf1e3b8f
SergiSolis <65497439+SergiSolis@users.noreply.github.com>
2021-06-14 15:29:41 +0200

    no entiendo del todo ex1

--- LAB 3/1axpy/axpy.c
+++ LAB 3/1axpy/axpy.c
@@ -8,13 +8,20 @@
 
 void axpy_cpu(int offset, int n, double alpha, double* x, double* y)
 {
-    
+
+    for (int i = offset; i < n; i++){
+       y[i] =  alpha * x[i] + y[i];
+    }
 
 }
 
 void axpy_gpu(int offset, int n, double alpha, double* x, double* y)
 {
     
+    #pragma acc parallel loop 
+    for (int i = offset; i < n; i++){
+       y[i] =  alpha * x[i] + y[i];
+    }
 
 }
 
